55a258cdd04e733f43f42a08533bff5da02bdab7
arashk yadegar <yadegar.arashk@gmail.com@>
2023-03-16 04:26:49 -0800

    bus class edited for blog.

--- app/blog/blogBus.ts
+++ app/blog/blogBus.ts
@@ -4,5 +4,23 @@ export interface BlogBus {
     find():Array<BlogEntity>; // returns Array of objects.
     findOne(id:number):BlogEntity; //returns found object.
     updateOne(id:number,b:BlogEntity):boolean; //returns updated object.
-    deleteOne(id:number):void;
+    deleteOne(id:number):boolean; //returns true if operation was successful othewise false.
+}
+
+export class BlogBusMongoDB implements BlogBus {
+    insertOne(b: BlogEntity): BlogEntity {
+        throw new Error('Method not implemented.');
+    }
+    find(): BlogEntity[] {
+        throw new Error('Method not implemented.');
+    }
+    findOne(id: number): BlogEntity {
+        throw new Error('Method not implemented.');
+    }
+    updateOne(id: number, b: BlogEntity): boolean {
+        throw new Error('Method not implemented.');
+    }
+    deleteOne(id: number): boolean {
+        throw new Error('Method not implemented.');
+    }
 }
