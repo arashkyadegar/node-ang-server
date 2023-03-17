9ba8ca5bb50d89b998411b3569dd30107093e377
arashk yadegar <yadegar.arashk@gmail.com@>
2023-03-17 12:31:01 -0800

    abstract class import change to interface

--- app/blog/blogBus.ts
+++ app/blog/blogBus.ts
@@ -1,4 +1,4 @@
-import {AbsBlog,BlogEntity} from './blogEntity'
+import {IBlog,BlogEntity} from './blogEntity'
 export interface BlogBus {
     insertOne(b:BlogEntity):BlogEntity; // returns created object.
     find():Array<BlogEntity>; // returns Array of objects.
