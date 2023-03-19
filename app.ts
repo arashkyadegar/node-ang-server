d31ff0a9d0c03472d2342d27e5eb614889727304
arashk yadegar <yadegar.arashk@gmail.com@>
2023-03-19 06:58:20 -0800

    db config added to app.ts

--- app.ts
+++ app.ts
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 var http=require('http');
 var express = require('express');
 //import  {router} from './app/routes';
-
+require('./app/config/db')();
 dotenv.config();
 const port = process.env.PORT;
 
 var app = express();
-
+app.use(express.json());
 require('./app/routes/index')(app);
 
 const server =http.createServer(app).listen(port, () => {
